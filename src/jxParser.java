53de2d9cc719f486c8c5992df0a5880e2f18a572
Garrett Haptonstall <ghaptonstall@gmail.com>
2015-12-17 11:42:07 -0800

    --add base json parse method via http call for standard api calls returning json objects.

--- src/jxParser.java
+++ src/jxParser.java
@@ -1,5 +1,84 @@
+package com.feardread.android.jxparser;
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.util.Log;
 
+public class JXParser {
 
+    final String TAG = "JXParser.java";
 
+    static String json = "";
+    static InputStream input = null;
+    static JSONObject jsonObj = null;
+
+    public JSONObject getJSON (String url) {
+
+        // make HTTP request
+        try {
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+
+            input = httpEntity.getContent();           
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+        // parse response
+        try {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+
+                sb.append(line + "\n");
+            }
+
+            input.close();
+            json = sb.toString();
+
+        } catch (Exception e) {
+
+            Log.e(TAG, "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+
+            jsonObj = new JSONObject(json);
+
+        } catch (JSONException e) {
+
+            Log.e(TAG, "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jsonObj;
+    }
+}
